57d9072f984c81b62034a2f0e96f32834abcdb86
Nguyen Vo <ntv4@cornell.edu>
2025-05-18 17:03:35 -0400

    Added basic tests for mouse functions

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from hardware_actions import Mouse;
 
 mouse = Mouse(100, 200);
+mouse.scroll(-1);
 mouse.left_click();
 mouse.right_click();
 mouse.move(25, 50);
+mouse.left_click_drag(75, 75);
+mouse.right_click_drag(50, 50);
