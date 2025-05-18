6074fd6599559f789a5a636c3221d6ddbc1fef35
Nguyen Vo <ntv4@cornell.edu>
2025-05-18 16:53:35 -0400

    Added basic mouse control macros

---
+++ install_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installing Python packages for gesture control..."
+
+pip3 install pyautogui
+
+echo "Installation complete!"
---
+++ test.py
@@ -0,0 +1,6 @@
+from hardware_actions import Mouse;
+
+mouse = Mouse(100, 200);
+mouse.left_click();
+mouse.right_click();
+mouse.move(25, 50);
